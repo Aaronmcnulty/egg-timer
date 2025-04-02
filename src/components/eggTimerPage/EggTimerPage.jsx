8c35ef61e1820b16eaaec82bca0ad81f2caab14b
malcomcrab <malcomcrabesq@outlook.com>
2025-04-02 15:34:58 +0100

    Feat: Basic timer countdown function added and buttons to change timer length

--- src/components/eggTimerPage/EggTimerPage.jsx
+++ src/components/eggTimerPage/EggTimerPage.jsx
@@ -1,14 +1,32 @@
+import { useEffect, useState } from "react";
 import Navbar from "../navigation/Navbar";
 import CountDownDisplay from "./CountdownDisplay";
 import TimerControls from "./TimerControls";
 
 function EggTimerPage(){
 
+    const [countdownStarted, setCountdownStarted] = useState(false)
+    const [timeRemaining, setTimeRemaining] = useState(120)
+    const [chosenTime, setChosenTime] = useState(0)
+    
+    useEffect(() => {
+       if(countdownStarted){
+        const key = setInterval(() => {
+            setTimeRemaining(timeRemaining => timeRemaining -1)
+        }, 1000)
+        
+        return () => {
+            clearInterval(key);
+          };
+       }
+    }, [countdownStarted])
+
     return(
         <div>
             <h1>Egg Page</h1>
             <CountDownDisplay />
-            <TimerControls />
+            <TimerControls setCountdownStarted={setCountdownStarted} setTimeRemaining={setTimeRemaining}/>
+            <p>{timeRemaining}</p>
         </div>
     )
 }
--- src/components/eggTimerPage/TimerControls.jsx
+++ src/components/eggTimerPage/TimerControls.jsx
@@ -1,15 +1,28 @@
 
-function TimerControls(){
+function TimerControls({setCountdownStarted, setTimeRemaining}){
 
     const handleStartClick = () => {
-        console.log('hi')
+        setCountdownStarted(true)
     }
 
+    const handleTimerClick = (event) => {
+        setTimeRemaining(event.target.value)
+    }
+
+    const handleStopClick = () => {
+        setCountdownStarted(false)
+    }
     return(
         <div>
 
+            <div>
+                <button value={120} onClick={handleTimerClick}>3:00</button>
+                <button value={150} onClick={handleTimerClick}>3:30</button>
+                <button value={180} onClick={handleTimerClick}>4:00</button>
+            </div>
+
             <button onClick={handleStartClick}>Start</button>
-            
+            <button onClick={handleStopClick}>Stop</button>
         </div>
     )
 }
