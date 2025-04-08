162353c32e2d16b133f3d81962d1bcdfcac2100a
malcomcrab <malcomcrabesq@outlook.com>
2025-04-08 17:12:15 +0100

    Feat: Adds cookingOptions select box and imports cooking instructions from module

--- src/components/instructionsPage/InstructionsPage.jsx
+++ src/components/instructionsPage/InstructionsPage.jsx
@@ -1,11 +1,28 @@
 import styles from "./cssModules/instructionsPage.module.css"
+import { cookingOptions } from "../eggTimerPage/cookingOptions";
+import { cookingInstructionsBoiled } from "../eggTimerPage/cookingOptions";
 
 
-function InstructionsPage(){
+function InstructionsPage({}){
+
+    const handleCookingChange = (event) => {
+        setCookingOption(cookingOptions[event.target.value]);
+      };
+
+
 
     return(
         <div>
+            <div className={styles.cookingSelectContainer}>
+                    <select onChange={handleCookingChange}>
+                      <option value={0}>BOILED</option>
+                      <option value={1}>POACHED</option>
+                    </select>
+            </div>
 
+            <div>
+                <p>{cookingInstructionsBoiled}</p>
+            </div>
         </div>
     )
 }
