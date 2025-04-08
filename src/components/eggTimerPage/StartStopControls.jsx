65174b6cb1b222c9617b8d2af2ebb454e18af1e5
malcomcrab <malcomcrabesq@outlook.com>
2025-04-08 16:32:06 +0100

    Feat: Adds confirm popup when starting the timer to remind the user the water should be boiling

--- src/components/eggTimerPage/StartStopControls.jsx
+++ src/components/eggTimerPage/StartStopControls.jsx
@@ -2,7 +2,13 @@ import styles from "./cssModules/startStopControls.module.css";
 
 function StartStopControls({ setCountdownStarted, setTimerFinished }) {
   const handleStartClick = () => {
-    setCountdownStarted(true);
+    let isBoiling = confirm('Is the water already boiling?')
+    
+    if(isBoiling == true){
+      setCountdownStarted(true)
+    } else {
+      alert('Make Sure the water is boiling before you start the timer')
+    }
   };
 
   const handleStopClick = () => {
