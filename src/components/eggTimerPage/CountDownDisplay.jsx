8182b59a91821a4fcb0a4b1ade292193add501bd
malcomcrab <malcomcrabesq@outlook.com>
2025-04-10 13:17:23 +0100

    Feat: Adds function to format seconds and minutes with leading zeros.

--- src/components/eggTimerPage/CountDownDisplay.jsx
+++ src/components/eggTimerPage/CountDownDisplay.jsx
@@ -4,19 +4,33 @@ import PropTypes from "prop-types";
 
 function CountDownDisplay({ minutesRemaining, secondsRemaining }) {
   const [correctSeconds, setCorrectSeconds] = useState("");
+  const [correctMinutes, setCorrectMinutes] = useState("");
 
   useEffect(() => {
+    formatSeconds()
+    formatMinutes()
+  }, [secondsRemaining, minutesRemaining]);
+
+  const formatSeconds = () => {
     if (secondsRemaining < 10) {
       setCorrectSeconds(String(secondsRemaining).padStart(2, 0));
     } else {
       setCorrectSeconds(secondsRemaining);
     }
-  }, [secondsRemaining]);
+  }
+
+  const formatMinutes = () => {
+    if (minutesRemaining < 10) {
+      setCorrectMinutes(String(minutesRemaining).padStart(2, 0));
+    } else {
+      setCorrectMinutes(minutesRemaining);
+    }
+  }
 
   return (
     <div  className={styles.timeContainer}>
       <p data-testid={'time-display'} className={styles.timeDisplay}>
-        {minutesRemaining}:{correctSeconds}
+        {correctMinutes}:{correctSeconds}
       </p>
     </div>
   );
